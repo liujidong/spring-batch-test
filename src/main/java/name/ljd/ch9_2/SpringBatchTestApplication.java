719a64c045dfee64ee383309c50136973d1a59ec
liujidong <wwwliujidong@163.com>
2018-12-14 16:17:52 +0800

    新建spring boot项目

---
+++ src/main/java/name/ljd/ch9_2/SpringBatchTestApplication.java
@@ -0,0 +1,14 @@
+package name.ljd.ch9_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/name/ljd/ch9_2/SpringBatchTestApplicationTests.java
@@ -0,0 +1,17 @@
+package name.ljd.ch9_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
